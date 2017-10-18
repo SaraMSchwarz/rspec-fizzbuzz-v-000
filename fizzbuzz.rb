50287f249e53ddfd77434e5ddf6ab7b11f12fbd4
Sara Schwarz <saramschwarz@gmail.com>
2017-10-18 23:17:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,9 @@
 def fizzbuzz(int)
 if int % 3 == 0
-  puts  "Fizz"
+  "Fizz"
 elsif int % 5 == 0
-  puts "Buzz"
+  "Buzz"
 elsif int % 5 == 0 && i % 3 == 0
-  puts "FizzBuzz"
+  "FizzBuzz"
 end
 end
-
-fizz_3 = fizzbuzz(3)
-fizz_5 = fizzbuzz(5)
